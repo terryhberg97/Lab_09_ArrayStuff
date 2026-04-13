e2fac709efbb201072f420c18e36e879ba5db60f
terryhberg97 <terryhberg97@gmail.com>
2026-04-13 15:51:14 -0400

    Completed Lab 09

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,81 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int dataPoints[] = new int[100];
+        Random rnd = new Random();
+        int val = rnd.nextInt(100) + 1;
+        int index = rnd.nextInt(dataPoints.length);
+        int sum = 0;
+        double avg = 0;
+        int count = 0;
+        int input = SafeInput.getRangedInt(in, "Enter an inclusive integer ", 1, 100);
+        int value = SafeInput.getRangedInt(in, "Enter another inclusive integer ", 1, 100);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        System.out.println("The number chosen is: " + input);
+
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            dataPoints[i] = rnd.nextInt(100) + 1;
+        }
+
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+        boolean found = false;
+
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            if (dataPoints[i] == input)
+            {
+                count++;
+            }
+            if (dataPoints[i] == value)
+            {
+                System.out.println();
+                System.out.println("The value " + value + " was found at array index " + i);
+                found = true;
+                break;
+            }
+            if (dataPoints[i] < min)
+            {
+                min = dataPoints[i];
+            }
+            if (dataPoints[i] > max)
+            {
+                max = dataPoints[i];
+            }
+            System.out.print(dataPoints[i] + " | ");
+            sum += dataPoints[i];
+        }
+        if (!found)
+        {
+            System.out.println();
+            System.out.println("The value " + value + " was not found");
+        }
+
+
+        avg = (double) sum / dataPoints.length;
+        System.out.println("The value " + input + " was found " + count + " times");
+        System.out.println("The average of the random array dataPoints is: " + avg);
+        System.out.println("The sum of the random array dataPoints is: " + sum);
+        System.out.println("The maximum value found is " + max + " and the minimum value found is " + min);
+        System.out.println("Average of dataPoints is: " + getAverage(dataPoints));
+
+    }
+
+    public static double getAverage(int values[]) {
+        int sum = 0;
+        for (int i = 0; i < values.length; i++)
+        {
+            sum += values[i];
+
+        }
+        return (double) sum / values.length;
     }
 }
+
+
+
